eb8d7aa7fc63709c7884379facef4d0354eed7d7
Lwarz117 <nihatsultanli5@gmail.com>
2024-10-12 20:40:20 +0400

    task1 2 complated

--- ilkdeneme/Program.cs
+++ ilkdeneme/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region task1
+            //int a = 5;
+            //int b = 6;
+            //int c = a + b;
+            //Console.WriteLine(c); 
+            #endregion
+            #region task2
+            //int a = 4;
+            //int b = 4;
+            //int c = a + b;
+            //Console.WriteLine(c);
+            #endregion
         }
     }
 }
